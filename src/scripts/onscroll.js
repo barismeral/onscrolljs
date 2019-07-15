f3829d00d765e776d92afaf44f3b31f1f2bb83d8
barismeral <30953081+barismeral@users.noreply.github.com>
2019-07-15 18:06:57 +0300

    version 1.0 upload

---
+++ src/scripts/onscroll.js
@@ -0,0 +1,69 @@
+'use strict';
+
+function onscroll() {
+
+    this.components;
+ 
+    this.start = () => {
+
+        window.addEventListener('load', this.initComponents);
+    
+        window.addEventListener('scroll', this.animate);
+
+    }
+
+    this.initComponents = () => {
+
+        this.components = document.querySelectorAll("[data-osc]");
+    
+        this.components.forEach(item => {
+
+            item.style.opacity = "0";
+
+            let distance = (item.dataset.oscDistance == undefined)? "50px" : item.dataset.oscDistance;
+
+            item.setAttribute("data-osc-origin",item.getBoundingClientRect().bottom);
+
+            switch (item.dataset.osc) {
+
+                    case "left":
+                    item.style.transform = `translateX(${distance})`;
+                    break;
+                    case "right":
+                    item.style.transform = `translateX(-${distance})`;
+                    break;
+                    case "top":
+                    item.style.transform = `translateY(-${distance})`;
+                    break;
+                    case "bottom":
+                    item.style.transform = `translateY(${distance})`;
+                    break;
+            }
+            
+        });
+
+        this.animate();
+ 
+    }
+
+    this.animate = () => {
+
+        this.components.forEach(item => {
+
+            let transition = (item.dataset.oscTransition == undefined ) ? "transform 2s" : item.dataset.oscTransition;
+
+            if (item.getBoundingClientRect().bottom < window.innerHeight) {
+
+                item.style.transition = "transform "+transition;
+                item.style.opacity = "1";
+                item.style.transform = "translate(0,0)";
+            }
+           
+        });
+            
+
+    }
+
+}
+
+
